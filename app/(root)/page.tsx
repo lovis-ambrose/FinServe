a6889278989e03078ae1f4d1c0e56f4b072d7218
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-27 15:45:15 +0300

    Fetch loggedIn bank credentials

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -13,7 +13,7 @@ const Home = async () =>{
                     <HeaderBox 
                         type="greeting"
                         title="Welcome"
-                        user={loggedIn?.name || 'Guest'}
+                        user={loggedIn?.lastName || 'Guest'}
                         subtext="Access and manage your account and transactions efficiently"
                     />
 
