8f079dc75ccd42b7d445fedcc05d4892faaf5971
lovis-ambrose <lovisambrose212@gmail.com>
2024-07-27 17:09:55 +0300

    fetch banks and balances

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,20 @@
 import HeaderBox from "@/components/HeaderBox";
 import RightSidebar from "@/components/RightSidebar";
 import TotalBalanceBox from "@/components/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
 import { getLoggedInUser } from "@/lib/actions/user.actions";
 import React from "react";
 
-const Home = async () =>{
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) =>{
     const loggedIn = await getLoggedInUser();
+    const accounts = await getAccounts({userId: loggedIn.$id});
+
+    if(!accounts) return;
+
+    const accountsData = accounts?.data;
+    const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+    const account = await getAccount({ appwriteItemId })
+    
     return (
         <section className="home">
             <div className="home-content">
@@ -18,18 +27,24 @@ const Home = async () =>{
                     />
 
                     <TotalBalanceBox 
-                        accounts={[]}
-                        totalBanks={1}
-                        totalCurrentBalance={1750.35}
+                        accounts={accountsData}
+                        totalBanks={accounts?.totalBanks}
+                        totalCurrentBalance={accounts?.totalCurrentBalance}
                     />
                 </header>
                 RECENT TRANSACTIONS
+                {/* <RecentTransactions 
+                accounts={accountsData}
+                transactions={account?.transactions}
+                appwriteItemId={appwriteItemId}
+                page={currentPage}
+                /> */}
             </div>
 
             <RightSidebar 
-                user = {loggedIn}
-                transactions = {[]}
-                banks = {[{currentBalance: 200.5}, {currentBalance: 125.35}]}
+                user={loggedIn}
+                transactions={account?.transactions}
+                banks={accountsData?.slice(0, 2)}
             />
         </section>
     )
--- components/DoughnutChart.tsx
+++ components/DoughnutChart.tsx
@@ -8,16 +8,18 @@ ChartJs.register(ArcElement, Tooltip, Legend);
 
 
 const DoughnutChart = ({accounts}: DoughnutChartProps) => {
+    const accountNames = accounts.map((a) => a.name);
+    const balances = accounts.map((a) => a.currentBalance)
+
     const data = {
         datasets: [{
             label: 'Bank',
+            // data: balances,
             data: [1250, 1100, 1456],
             backgroundColor: ['#0747b6', '#2265d8', '#2f91fa'],
         }],
         labels: [
-            'Bank 1',
-            'Bank 2',
-            'Bank 3'
+            accountNames
         ]
     }
 
