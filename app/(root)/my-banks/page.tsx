5531f7f7021e4bbeac7f832765ed86b2a2ddcf75
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-01 15:41:16 +0300

    small fix

--- app/(root)/my-banks/page.tsx
+++ app/(root)/my-banks/page.tsx
@@ -20,14 +20,14 @@ const MyBanks = async () => {
 
         <div className="space-y-4">
           <h2 className="header-2">
-            Your cards
+            My cards
           </h2>
           <div className="flex flex-wrap gap-6">
             {accounts && accounts.data.map((a: Account) => (
               <BankCard 
                 key={accounts.id}
                 account={a}
-                userName={loggedIn?.firstName}
+                userName={`${loggedIn?.firstName} ${loggedIn.lastName}`}
               />
             ))}
           </div>
