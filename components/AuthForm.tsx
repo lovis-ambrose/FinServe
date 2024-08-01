012f35074d5920eb53331de163646341b6fca8a5
lovis-ambrose <lovisambrose212@gmail.com>
2024-08-01 17:56:01 +0300

    small fix

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -90,7 +90,7 @@ const AuthForm = ({ type }: { type: string }) => {
               src="/icons/logo.svg"
               width={34}
               height={34}
-              alt="Horizon logo"
+              alt="finserve logo"
             />
             <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">FinServe</h1>
           </Link>
